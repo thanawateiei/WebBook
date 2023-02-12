0507dd68570a3610a08accbffc6ee9380679aff1
thanawateiei <122959522+thanawateiei@users.noreply.github.com>
2023-02-12 20:38:04 +0700

    up date 2-12

---
+++ WebBook/ViewModels/AgencyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBook.ViewModels
+{
+    public class AgencyViewModel
+    {
+        [Key]
+        public int AgencyId { get; set; }
+        public string? AgencyName { get; set; }
+
+    }
+
+
+}
---
+++ WebBook/ViewModels/UserTypeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBook.ViewModels
+{
+    public class UserTypeViewModel
+    {
+        [Key]
+        public int UserTypeId { get; set; }
+        public int? Limit { get; set; }
+        public string? UserTypeName { get; set; }
+
+    }
+
+
+}
