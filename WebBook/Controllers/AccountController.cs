f7e47607cac374c5e2dd43f33a614d321293cb5a
ThanakornPao <thanakorn.nittayaprapa@gmail.com>
2023-02-21 23:48:59 +0700

    User Login and Register

--- WebBook/Controllers/AccountController.cs
+++ WebBook/Controllers/AccountController.cs
@@ -1,23 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using WebBook.Models;
-using WebBook.ViewModels;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebBook.Controllers
 {
-    public class AdminController : Controller
+    public class AccountController : Controller
     {
         private readonly webContext _db;
-        public AdminController(webContext db)
+        public AccountController(webContext db)
         { _db = db; }
-
-   
         public IActionResult Index()
         {
-            if (HttpContext.Session.GetInt32("UserId") == null || HttpContext.Session.GetInt32("UserRole") != 1)
-            {
-                return RedirectToAction("Login");
-            }
             return View();
         }
         public IActionResult Login()
@@ -31,31 +24,42 @@ namespace WebBook.Controllers
             //Query หาว่ามี Login Password ที่ระบุหรือไม่
             var user = from u in _db.Users
                        where u.Email.Equals(userEmail)
-                             && u.Password.Equals(userPass)
                        select u;
+            var userinfo = _db.Users.SingleOrDefault(u => u.Email == userEmail);
+
+            bool isValidPassword = BCrypt.Net.BCrypt.Verify(userPass, userinfo.Password);
+            // var cus = _db.Customers.Find(userName);
             //ถ้าข้อมูลเท่ากับ 0 ให้บอกว่าหาข้อมูลไม่พบ
             if (user.ToList().Count == 0)
             {
                 //ถ้าใช้ RedirectToAction ไม่สามารถใช้ ViewBag ได้ ต้องใช้ TempData
-                TempData["ErrorMessage"] = "ระบุผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
+                TempData["ErrorMessage"] = "ไม่พบผู้ใช้";
                 //ViewBag.ErrorMessage = "ระบุผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
-                return View();
+                return RedirectToAction("Index");
             }
-
+            if (!isValidPassword)
+            {
+                TempData["ErrorMessage"] = "รหัสผ่านผิด";
+                return RedirectToAction("Index");
+            }
             //ถ้าหาข้อมูลพบ ให้เก็บค่าเข้า Session 
             int UserId;
             string UserEmail;
             int UserRole;
+            int UserType;
             foreach (var item in user)
             {
                 //อ่านค่าจาก Object เข้าตัวแปร
                 UserId = item.UserId;
                 UserEmail = item.Email;
                 UserRole = (int)item.Role;
+                UserType = (int)item.UserType;
+
                 //เอาค่าจากตัวแปรเข้า Session
                 HttpContext.Session.SetInt32("UserId", UserId);
                 HttpContext.Session.SetString("UserEmail", UserEmail);
                 HttpContext.Session.SetInt32("UserRole", UserRole);
+                HttpContext.Session.SetInt32("UserType", UserType);
                 //Update  Column ของตารางที่ระบุ
                 //โดยทำการอ่านค่าตาม รหัส หรือ id ที่ระบุ
                 //และระบุค่าแต่ละ Field ของ Record นั้นๆ
@@ -69,7 +73,41 @@ namespace WebBook.Controllers
             //ทำการบันทึกทุก Record ที่สั่ง Modified ไว้
             _db.SaveChanges();
             //ทำการย้ายไปหน้าที่ต้องการ
-            return RedirectToAction("Index");
+            return RedirectToAction("Index","Home");
+        }
+        public IActionResult Register()
+        {
+            var idUser = from u in _db.Users
+                             select u.UserId;
+            var maxUserId = idUser.Max();
+            ViewBag.UserId = maxUserId + 1;
+            return View();
+        }
+        [HttpPost] //ระบุว่าเป็นการทำงานแบบ Post
+        [ValidateAntiForgeryToken] // ป้องกันการโจมตี Cross_site Request Forgery
+        //ค่าที่ส่งมาจาก Form เป็น Object ของ Model ที่ระบุ ตัว Controller ก็รับค่าเป็น Object
+        public IActionResult Register(User obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    obj.Role = 3;
+                    obj.Password = BCrypt.Net.BCrypt.HashPassword(obj.Password);
+                    _db.Users.Add(obj); //ส่งคำสั่ง Add ผ่าน DBContext
+                    _db.SaveChanges(); // Execute คำสั่ง
+                    return RedirectToAction("Login"); // ย้ายทำงาน Action Index
+                }
+            }
+            catch (Exception ex)
+            {
+                //ถ้าไม่ Valid ก็ สร้าง Error Message ขึ้นมา แล้ว ส่ง Obj กลับไปที่ View
+                ViewBag.ErrorMessage = ex.Message;
+                return View(obj);
+            }
+            //ถ้าไม่ Valid ก็ สร้าง Error Message ขึ้นมา แล้ว ส่ง Obj กลับไปที่ View
+            ViewBag.ErrorMessage = "การบันทึกผิดพลาด";
+            return View(obj);
         }
         public IActionResult Logout()
         {
@@ -77,6 +115,5 @@ namespace WebBook.Controllers
             HttpContext.Session.Clear();
             return RedirectToAction("Index");
         }
-
     }
 }
--- WebBook/Controllers/HomeController.cs
+++ WebBook/Controllers/HomeController.cs
@@ -16,60 +16,6 @@ namespace WebBook.Controllers
         {
             return View();
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Login(string userEmail, string userPass)
-        {
-            //Query หาว่ามี Login Password ที่ระบุหรือไม่
-            var user = from u in _db.Users
-                       where u.Email.Equals(userEmail)
-                             && u.Password.Equals(userPass)
-                       select u;
-
-            // var cus = _db.Customers.Find(userName);
-            //ถ้าข้อมูลเท่ากับ 0 ให้บอกว่าหาข้อมูลไม่พบ
-            if (user.ToList().Count == 0)
-            {
-                //ถ้าใช้ RedirectToAction ไม่สามารถใช้ ViewBag ได้ ต้องใช้ TempData
-                TempData["ErrorMessage"] = "ระบุผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
-                //ViewBag.ErrorMessage = "ระบุผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
-                return View();
-            }
-
-            //ถ้าหาข้อมูลพบ ให้เก็บค่าเข้า Session 
-            int UserId;
-            string UserEmail;
-            foreach (var item in user)
-            {
-                //อ่านค่าจาก Object เข้าตัวแปร
-                UserId = item.UserId;
-                UserEmail = item.Email;
-                //เอาค่าจากตัวแปรเข้า Session
-                HttpContext.Session.SetInt32("UserId", UserId);
-                HttpContext.Session.SetString("UserEmail", UserEmail);
-                //Update  Column ของตารางที่ระบุ
-                //โดยทำการอ่านค่าตาม รหัส หรือ id ที่ระบุ
-                //และระบุค่าแต่ละ Field ของ Record นั้นๆ
-                //แล้วกำหนดให้ทำการปรับเปลี่ยน (Modified)
-                var theRecord = _db.Users.Find(UserId);
-                //theRecord.LastLogin = DateTime.Now.Date;
-                //theRecord.CusAdd = "ABCD";
-                //theRecord.CusEmail = "a@b.com";
-                //_db.Entry(theRecord).State = EntityState.Modified;
-            }
-            //ทำการบันทึกทุก Record ที่สั่ง Modified ไว้
-            _db.SaveChanges();
-            //ทำการย้ายไปหน้าที่ต้องการ
-            return RedirectToAction("Index");
-        }
-        public IActionResult Logout()
-        {
-            //ล้างทุก Session และย้ายกลับหน้า Index
-            HttpContext.Session.Clear();
-            return RedirectToAction("Index");
-        }
-
         public IActionResult Privacy()
         {
             return View();
