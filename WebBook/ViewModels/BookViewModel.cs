efb9e39260d252effe33b2bc4f3125f03b32985e
thanawateiei <122959522+thanawateiei@users.noreply.github.com>
2023-02-24 21:38:15 +0700

    2-24 เริมเป็นเว็บแล้วฮะ

--- WebBook/ViewModels/BookViewModel.cs
+++ WebBook/ViewModels/BookViewModel.cs
@@ -12,9 +12,19 @@ namespace WebBook.ViewModels
         public int? PublicationYear { get; set; }
         public string? Publisher { get; set; }
         public string? BookCover { get; set; }
+        public IFormFile? Bookimg { get; set; }
         public string? BookDetail { get; set; }
-        public List<BackCBTViewModel> CheckBT { get; set; }
-      
+        public int? BookType1 { get; set; }
+        public int? BookType2 { get; set; }
+        public int? BookType3 { get; set; }
+        public int? BookType4 { get; set; }
+        public int? BookType5 { get; set; }
+        public int? BookType6 { get; set; }
+        public int? BookType7 { get; set; }
+        public int? BookType8 { get; set; }
+        public int? BookType9 { get; set; }
+        public int? BookType10 { get; set; }
+
 
     }
     public class CBTViewModel
