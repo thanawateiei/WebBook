2b22c2470b0d7d51c8cfd884c2be113840ae9db0
ThanakornPao <thanakorn.nittayaprapa@gmail.com>
2023-03-10 00:12:57 +0700

    add History page

---
+++ WebBook/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebBook.ViewModels
+{
+    public class HistoryViewModel
+    {
+            [Key]
+            public string HistoryId { get; set; }
+            public string? UserEmail { get; set; }
+            public string? BookTitle { get; set; }
+            public string? CallNumber { get; set; }
+            public string? Location { get; set; }
+            public string? ReceiveDate { get; set; }
+            public string? ReturnDate { get; set; }
+            public string? Status { get; set; }
+
+
+    }
+
+}
+
